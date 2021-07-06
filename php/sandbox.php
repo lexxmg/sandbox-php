07a235437a2372e3ff754c6c0b7e946bf3e9ef67
mac-mini <lexx.mg@gmail.com>
2021-07-06 21:06:05 +0300

    fix format

--- php/sandbox.php
+++ php/sandbox.php
@@ -2,12 +2,12 @@
 <?php
 
 $result1 = [
-  'author' => [
-    'fullName' => 'Достоевский Фёдор Михайлович',
-    'email' => 'Dostoevsky@fake.net'
-  ],
-  'book' => [
-    'name' => 'Преступление и наказание',
-    'email' => 'Dostoevsky@fake.net'
-  ]
+    'author' => [
+        'fullName' => 'Достоевский Фёдор Михайлович',
+        'email' => 'Dostoevsky@fake.net'
+    ],
+    'book' => [
+        'name' => 'Преступление и наказание',
+        'email' => 'Dostoevsky@fake.net'
+    ]
 ];
