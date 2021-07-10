233d3769abfb51424e0954e4497924d7673ee8fd
mac-mini <lexx.mg@gmail.com>
2021-07-10 06:41:30 +0300

    sync

--- index.php
+++ index.php
@@ -24,19 +24,46 @@
 
   <body>
     <div class="fixed-container">
-      <h1>POSR & GET</h1>
+      <h1 class="title">POSR & GET</h1>
 
-      <p>Массив GET</p>
+      <p class="par">Массив GET</p>
 
       <pre>
         <?php var_dump($_GET)?>
       </pre>
 
-      <p>Массив POST</p>
+      <p class="par">Массив POST</p>
 
       <pre>
         <?php var_dump($_POST)?>
       </pre>
+
+      <p><?=$_SERVER['DOCUMENT_ROOT']?></p>
+
+      <form action="$_SERVER('DOCUMENT_ROOT')" class="form" method="post" name="myForm">
+        <div class="form__input-wrapper form-input-wrapper">
+          <input class="form-input-wrapper__input" id="number1" type="text" name="number1">
+          <label for="number1" class="form-input-wrapper__label">Введите число</label>
+        </div>
+
+        <div class="form__input-wrapper form-input-wrapper">
+          <input class="form-input-wrapper__input" id="number2" type="text" name="number2">
+          <label for="number2" class="form-input-wrapper__label">Введите число</label>
+        </div>
+
+        <div class="form__input-wrapper form-input-wrapper">
+          <input class="form-input-wrapper__input" id="number3" type="text" name="number3">
+          <label for="number3" class="form-input-wrapper__label">Введите число</label>
+        </div>
+
+        <select class="form__select" name="myFormSelect">
+          <option selected value="">Не сортировать</option>
+          <option value="desc">По убыванию</option>
+          <option value="asc">По возрастанию</option>
+        </select>
+
+        <button class="form__btn" name="myFormSubmit" value="send">Вычислить</button>
+      </form>
     </div>
   </body>
 </html>
