ef7cedd2d4e437e917af64951eabb1bc539dec32
mac-book <lexx.mg@gmail.com>
2022-06-23 21:34:20 +0300

    first Commit

--- php/sandbox.php
+++ php/sandbox.php
@@ -9,6 +9,52 @@
 
 // private: свойства и методы с данным модификатором доступны только из текущего класса
 
+/**
+ * Здания
+ */
+class Building
+{
+    const MAX_FLOORS = 20;
+
+    private $floors;
+
+    public function setFloorsNumber($floorNumber)
+    {
+        echo 'Вызван метод ' . __METHOD__ . PHP_EOL;
+
+        if ($floorNumber > self::MAX_FLOORS) {
+            echo 'Слишком высокое!' . PHP_EOL;
+            return;
+        }
+
+        $this->floors = $floorNumber;
+    }
+
+    public function showFloorNumber()
+    {
+        echo $this->floors . PHP_EOL;
+    }
+
+    public function showClossName()
+    {
+        echo 'Имя данного класса ' . __CLASS__ . PHP_EOL;
+        self::static();
+    }
+
+    public static function static()
+    {
+        echo 'Статическая функция' . PHP_EOL;
+    }
+}
+
+$building = new Building();
+$building->setFloorsNumber(7);
+$building->setFloorsNumber(21);
+$building->showFloorNumber();
+$building->showClossName();
+echo Building::class . PHP_EOL;
+Building::static();
+
 
 /**
  *
@@ -48,39 +94,7 @@ $employee_1_1->gender = 'gerl';
 $employee_1_1->name = 'Alisiay';
 $employee_1_1->surname = 'africa';
 $employee_1_1->position = 'CEO';
-$employee_1_1->displayAge();
-$employee_1_1->all();
-$employee_1_1->changePosition('NEW');
-$employee_1_1->all();
-
-
-$employee_1 = [
-    'age' => 18,
-    'gende' => 'gerl',
-    'name' => 'Alisiay',
-    'surname' => 'africa',
-    'position' => 'CEO'
-];
-
-$employee_2 = [
-    'age' => 38,
-    'gende' => 'male',
-    'name' => 'Vasya',
-    'surname' => 'Ivanov',
-    'position' => 'CTO'
-];
-
-function displayName($employee)
-{
-    echo $employee['name'];
-}
-
-function displayAge($employee)
-{
-    echo $employee['age'];
-}
-
-function changePosition(&$employee, $position)
-{
-    $employee['position'] = $position;
-}
+// $employee_1_1->displayAge();
+// $employee_1_1->all();
+// $employee_1_1->changePosition('NEW');
+// $employee_1_1->all();
